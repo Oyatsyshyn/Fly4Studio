fc41b6d1caa4cf0e5e2ac36d63d905b56f90a996
Oyatsyshyn <60934599+Oyatsyshyn@users.noreply.github.com>
2021-05-26 12:01:46 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const toggle = () => {
+  document.getElementById("lineUpper").classList.toggle("LineUpperToggled");
+  document.getElementById("lineCenter").classList.toggle("LineCenterToggled");
+  document.getElementById("lineLower").classList.toggle("LineLowerToggled");
+  document.getElementById("HeaderMenu").classList.toggle("Active");
+};
+const toggleContactUS = () => {
+  document.getElementById("ContactUS").classList.toggle("ContactUSToggled");
+};
